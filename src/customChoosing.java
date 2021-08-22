e6f418a78168dd660dde16465e1afee7ddb384b2
Jasmine-Mao <87322165+Jasmine-Mao@users.noreply.github.com>
2021-08-22 17:18:36 -0400

    Update customChoosing.java
    Added comments

--- src/customChoosing.java
+++ src/customChoosing.java
@@ -1,3 +1,4 @@
+//Frame that allows a user to choose what kind of questions they want and how large the numbers will be
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Image;
@@ -54,60 +55,71 @@ public class customChoosing extends JFrame {
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("Background.png")).getImage();
 
-		JLabel customLabel = new JLabel("Make your own custom level!");
-		customLabel.setForeground(new Color(250, 128, 114));
-		customLabel.setFont(new Font("Cooper Black", Font.PLAIN, 30));
-		customLabel.setBounds(20, 20, 688, 105);
+		//Title that tells the user they can make a custom level
+		JLabel customLabel = new JLabel("Make your own custom level!"); //Title in a JLabel
+		customLabel.setForeground(new Color(250, 128, 114)); //Set text colour
+		customLabel.setFont(new Font("Cooper Black", Font.PLAIN, 30)); //Set font and Style
+		customLabel.setBounds(20, 20, 688, 105); //Sets where the title is on the frame
 		contentPane.add(customLabel);
 
+		//Checkbox for addition
 		JCheckBox addCheckBox = new JCheckBox("Addition");
 		addCheckBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				addition = true;
+			//When the user checks this box, the variable addition will be made true. Will become pertinent when the level is made in the play frame
 			}
 		});
-		addCheckBox.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		addCheckBox.setBounds(30, 131, 257, 55);
+		addCheckBox.setFont(new Font("Cooper Black", Font.PLAIN, 20)); //Set font and style of the checkbox
+		addCheckBox.setBounds(30, 131, 257, 55); //Sets the checkbox in the frame
 		contentPane.add(addCheckBox);
 
+		//Checkbox for subtraction
 		JCheckBox subtractCheckBox = new JCheckBox("Subtraction");
 		subtractCheckBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				subtraction = true;
+			//When the user checks this box, the variable subtraction will be made true. Will become pertinent when the level is made in the play frame
 			}
 		});
-		subtractCheckBox.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		subtractCheckBox.setBounds(30, 195, 257, 55);
+		subtractCheckBox.setFont(new Font("Cooper Black", Font.PLAIN, 20)); //Set font and style of the checkbox
+		subtractCheckBox.setBounds(30, 195, 257, 55); //Sets the checkbox in the frame
 		contentPane.add(subtractCheckBox);
 
+		//Adds a JSpinner that allows the user to choose the largest number that can be used. Ex. if the user puts in 100, the highest number that can be used is 100.
 		JSpinner upperBound = new JSpinner();
 		upperBound.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				upperNum = (Integer) upperBound.getValue();
+			//When the spinner is given a value, that value is made the upper bound. This will become pertinent in the plau frame, when numbers will be randomized for the arithmetic
 			}
 		});
-		upperBound.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		upperBound.setBounds(219, 306, 125, 48);
+		upperBound.setFont(new Font("Cooper Black", Font.PLAIN, 20)); //Set font and style
+		upperBound.setBounds(219, 306, 125, 48); //Sets the spinner in the frame
 		contentPane.add(upperBound);
 
+		//Label beside the JSpinner that tells the user to put in the highest number
 		JLabel rangeLabel = new JLabel("Highest Number: ");
-		rangeLabel.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		rangeLabel.setBounds(34, 296, 407, 69);
+		rangeLabel.setFont(new Font("Cooper Black", Font.PLAIN, 20)); //Set font and style
+		rangeLabel.setBounds(34, 296, 407, 69); //Sets the label in the frame
 		contentPane.add(rangeLabel);
 
+		//Go button
 		JButton goButton = new JButton("GO!");
 		goButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				play screenPlay = new play();
 				screenPlay.setVisible(true);
 				customChoosing.this.setVisible(false);
+			//When the GO button is pressed, the current frame will close and the play frame will open
 			}
 		});
-		goButton.setFont(new Font("Cooper Black", Font.PLAIN, 20));
-		goButton.setBackground(Color.RED);
-		goButton.setBounds(666, 410, 103, 69);
+		goButton.setFont(new Font("Cooper Black", Font.PLAIN, 20)); //Set font and style
+		goButton.setBackground(Color.RED); //Sets the text colour
+		goButton.setBounds(666, 410, 103, 69); //Sets the background colour
 		contentPane.add(goButton);
 
+		//Blank label for the background image
 		JLabel descriptionLabel = new JLabel("");
 		descriptionLabel.setBounds(0, 0, 900, 600);
 		contentPane.add(descriptionLabel);
